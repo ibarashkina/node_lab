dd483dc0ce1c84de658c20aed1d05787c6060e9d
Ilona <ilonabarashkina@gmail.com>
2019-05-20 19:31:08 -0400

    Changed the formatting

--- quotes.js
+++ quotes.js
@@ -1,5 +1,5 @@
 
-const bestQuotes = ["One of my most productive days was throwing away 1000 lines of code. - Ken Thompson", "Don't comment bad code - rewrite it. - Brian Kernighan", "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it. - Patrick McKenzie","The way to get started is to quit talking and begin doing. - Walt Disney","We generate fears while we sit. We overcome them by action. - Dr.Henry Link","You are never too old to set another goal or to dream a new dream. - C.S. Lewis"];
+const bestQuotes = ["One of my most productive days was throwing away 1000 lines of code. - Ken Thompson", "Don't comment bad code - rewrite it. - Brian Kernighan", "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it. - Patrick McKenzie","The way to get started is to quit talking and begin doing. - Walt Disney","We generate fears while we sit. We overcome them by action. - Dr. Henry Link","You are never too old to set another goal or to dream a new dream. - C.S. Lewis"];
 
 module.exports = {
     bestQuotes
